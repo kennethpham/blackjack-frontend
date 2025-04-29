e4cfbbb7531447baf42593ae0056e7025eb391f1
Kenneth Pham <kennethpham98@gmail.com>
2025-04-28 23:55:42 -0700

    fix nav bar to be centered correctly

--- src/routes/+layout.svelte
+++ src/routes/+layout.svelte
@@ -10,17 +10,25 @@
 
 <div>
   {#if $currUser.name != ''}
-    <nav class="flex p-5 justify-between">
-      <p class="text-white bg-gray-800 rounded-full py-2 px-3">
-        {`Logged in as: ${$currUser.name}`}
-      </p>
-      <p class="text-white bg-gray-800 rounded-full py-2 px-3">Blackjack</p>
-      <button
-        class="text-white text-sm bg-gray-900 hover:bg-gray-700 rounded-full cursor-pointer"
-        on:click={logout}
-      >
-        <p class="py-2 px-3">Logout</p>
-      </button>
+    <nav class="flex p-5">
+      <div class="w-screen text-left">
+        <p class="w-fit text-white bg-gray-800 rounded-full py-2 px-3">
+          {`Logged in as: ${$currUser.name}`}
+        </p>
+      </div>
+      <div class="w-screen text-center">
+        <p class="w-fit m-auto text-white bg-gray-800 rounded-full py-2 px-3">
+          {'Blackjack'}
+        </p>
+      </div>
+      <div class="w-screen text-right">
+        <button
+          class="w-fit text-white text-sm bg-gray-900 hover:bg-gray-700 rounded-full cursor-pointer"
+          on:click={logout}
+        >
+          <p class="py-2 px-3">{'Logout'}</p>
+        </button>
+      </div>
     </nav>
   {/if}
   <slot />
